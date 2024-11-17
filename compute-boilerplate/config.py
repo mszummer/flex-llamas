43468b17539a9500d08a43c07b6726b56d525a4f
Martin Szummer <szummer@igent.ai>
2024-11-17 20:10:31 +0000

    Set model in config.py to Llama 3B

--- compute-boilerplate/config.py
+++ compute-boilerplate/config.py
@@ -4,8 +4,9 @@ import os
 dotenv.load_dotenv()
 
 class Config:
-    MODEL_NAME: str = "openai-community/gpt2"
+    # MODEL_NAME: str = "meta-llama/Llama-3.2-11B-Vision-Instruct"
+    MODEL_NAME: str = "meta-llama/Llama-3.2-3B-Instruct"
     PORT: int = 8000
-    DEFAULT_MAX_TOKENS: int = 100
+    DEFAULT_MAX_TOKENS: int = 10000
     DEFAULT_TEMPERATURE: float = 1
     HUGGINGFACE_ACCESS_TOKEN: str = os.getenv("HUGGINGFACE_ACCESS_TOKEN", "")
